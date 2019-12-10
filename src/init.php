4a1cf1a10a34ad0ba1df444ec2195b62224e537a
bethojunior <hgomescostajr@gmail.com>
2019-12-09 23:46:17 -0300

    correcao ma pagina erta

--- src/init.php
+++ src/init.php
@@ -1,6 +1,6 @@
 <?php
-
-$init = exec("php artisan make:command createView");
+$init = exec('cd .. && cd .. && cd .. && cd .. && php artisan make:command teste2');
+//$init = exec("php artisan make:command createView");
 //troca o arquivo gerado pelo View.php
 $path = './app/Console/Command/createView.php';
 $file = './src/View.php';
