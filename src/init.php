0b6a1f8c82c825dc28afc98dc2e9b015a7275b94
bethojunior <hgomescostajr@gmail.com>
2019-12-10 08:52:32 -0300

    create command laravel in progress

--- src/init.php
+++ src/init.php
@@ -1,6 +1,5 @@
 <?php
-$init = exec("cd .. && cd .. && cd ..  && cd .. && php artisan make:command madgicView");
-//$init = exec("php artisan make:command madgicView");
+$init = exec("php artisan make:command madgicView");
 //troca o arquivo gerado pelo View.php
 $path = './app/Console/Command/madgicView.php';
 $file = './src/View.php';
