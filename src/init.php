60179ac1ef9ce4ef828cb1ee65da84ae9ea74d36
bethojunior <hgomescostajr@gmail.com>
2019-12-09 23:51:26 -0300

    correcao script

--- src/init.php
+++ src/init.php
@@ -1,5 +1,5 @@
 <?php
-$init = exec('cd .. && cd .. && cd .. && cd .. && php artisan make:command teste2');
+$init = exec('cd .. && cd .. && cd .. && cd .. && php artisan make:command madgicteste');
 //$init = exec("php artisan make:command createView");
 //troca o arquivo gerado pelo View.php
 $path = './app/Console/Command/createView.php';
