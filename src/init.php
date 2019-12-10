0d5b3a80b13db07f03971b4366c8b5b0bccc87c6
bethojunior <hgomescostajr@gmail.com>
2019-12-09 23:33:49 -0300

    ok

--- src/init.php
+++ src/init.php
@@ -4,4 +4,4 @@ $init = exec("php artisan make:command createView");
 //troca o arquivo gerado pelo View.php
 $path = './app/Console/Command/createView.php';
 $file = './src/View.php';
-copy($file,$path);
+//copy($file,$path);
